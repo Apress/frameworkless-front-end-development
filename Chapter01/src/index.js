6d1ba02260c83a5077309bce7330f6a64e43e9f9
francesco-strazzullo <francesco.strazzullo86@gmail.com>
2018-10-21 14:55:33 +0200

    basic first React example

---
+++ Chapter01/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello Parcel</h1>
+    </div>
+  )
+}
+
+render(<App />, document.getElementById('app'))
