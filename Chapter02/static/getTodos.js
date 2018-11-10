16311193e9fb99ea2604b5f5b920302b2b5f9248
francesco-strazzullo <francesco.strazzullo86@gmail.com>
2018-11-10 17:32:00 +0100

    random elements

--- Chapter02/static/getTodos.js
+++ Chapter02/static/getTodos.js
@@ -1,5 +1,16 @@
-export default () => [
-  {
-    text: 'Hello Frameworkless'
+const ELEMENT = {
+  text: 'Hello Frameworkless'
+}
+
+const repeat = (element, number) => {
+  const array = []
+  for (let index = 0; index < number; index++) {
+    array.push(element)
   }
-]
+  return array
+}
+
+export default () => {
+  const howMany = Math.floor((Math.random() * 10) + 1)
+  return repeat(ELEMENT, howMany)
+}
