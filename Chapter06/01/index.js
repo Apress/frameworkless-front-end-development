6e37d7c7e1153a23c23c571fe6d234cd059dfca7
francesco-strazzullo <francesco.strazzullo86@gmail.com>
2019-03-28 20:44:06 +0100

    history API routing

---
+++ Chapter06/01/index.js
@@ -0,0 +1,24 @@
+import createRouter from './router.js'
+import createPages from './pages.js'
+
+const container = document.querySelector('main')
+
+const pages = createPages(container)
+
+const router = createRouter()
+
+router
+  .addRoute('/', pages.home)
+  .addRoute('/list', pages.list)
+  .addRoute('/list/:id', pages.detail)
+  .addRoute('/list/:id/:anotherId', pages.anotherDetail)
+  .setNotFound(pages.notFound)
+  .start()
+
+document
+  .querySelectorAll('button[data-navigate]')
+  .forEach(b => {
+    b.addEventListener('click', (e) => {
+      router.navigate(b.dataset.navigate)
+    })
+  })
