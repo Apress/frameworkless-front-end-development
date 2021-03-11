ad2a10f0602fc60b3274e0b7c95edd3633fc26df
daadaadaah <daadaadaah@gmail.com>
2021-03-11 13:22:18 +0900

    [문자열 사용 방지 방법]Template 사용

--- Chapter03/practice/view/todos.js
+++ Chapter03/practice/view/todos.js
@@ -1,28 +1,45 @@
+let template
+
+const createNewTodoNode = () => {
+  if (!template) {
+    template = document.getElementById('todo-item')
+  }
+
+  return template
+    .content
+    .firstElementChild
+    .cloneNode(true)
+}
+
 const getTodoElement = todo => {
   const {
     text,
     completed
   } = todo
 
-  return `
-      <li ${completed ? 'class="completed"' : ''}>
-        <div class="view">
-          <input 
-            ${completed ? 'checked' : ''}
-            class="toggle" 
-            type="checkbox">
-          <label>${text}</label>
-          <button class="destroy"></button>
-        </div>
-        <input class="edit" value="${text}">
-      </li>`
+  const element = createNewTodoNode()
+
+  element.querySelector('input.edit').value = text
+  element.querySelector('label').textContent = text
+
+  if (completed) {
+    element.classList.add('completed')
+    element.querySelector('input.toggle').checked = true
+  }
+
+  return element
 }
 
 export default (targetElement, { todos }) => {
   const newTodoList = targetElement.cloneNode(true)
-  const todosElements = todos
+
+  newTodoList.innerHTML = ''
+
+  todos
     .map(getTodoElement)
-    .join('')
-  newTodoList.innerHTML = todosElements
+    .forEach(element => {
+      newTodoList.appendChild(element)
+    })
+
   return newTodoList
 }
