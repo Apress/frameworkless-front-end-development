5774cca4a2cab6cd60c69c9bbf19aa27cd7e37f6
daadaadaah <daadaadaah@gmail.com>
2021-03-11 13:15:36 +0900

    기존 코드

---
+++ Chapter03/practice/index.js
@@ -0,0 +1,26 @@
+import getTodos from './getTodos.js'
+import todosView from './view/todos.js'
+import counterView from './view/counter.js'
+import filtersView from './view/filters.js'
+import applyDiff from './applyDiff.js'
+
+import registry from './registry.js'
+
+registry.add('todos', todosView)
+registry.add('counter', counterView)
+registry.add('filters', filtersView)
+
+const state = {
+  todos: getTodos(),
+  currentFilter: 'All'
+}
+
+const render = () => {
+  window.requestAnimationFrame(() => {
+    const main = document.querySelector('.todoapp')
+    const newMain = registry.renderRoot(main, state)
+    applyDiff(document.body, main, newMain)
+  })
+}
+
+render()
